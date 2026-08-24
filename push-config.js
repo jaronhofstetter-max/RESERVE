0e077feb5d929a88a75b5f572087a215f6ac3400
TenderHawk <jaronhofstetter@gmail.com>
2026-08-24 18:29:24 +0200

    Add TenderHawk web-push configuration scaffold

---
+++ push-config.js
@@ -0,0 +1,6 @@
+// TenderHawk Web Push configuration.
+// Public values only. Never place VAPID private keys or service-role secrets here.
+window.TENDERHAWK_PUSH_CONFIG = {
+  vapidPublicKey: '',
+  subscribeEndpoint: ''
+};
